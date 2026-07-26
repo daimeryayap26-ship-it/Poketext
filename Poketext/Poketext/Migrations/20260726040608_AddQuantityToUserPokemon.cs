5820f41693f86b64f2b88486baaa849a1ebc67f3
BORROMEO, RONALYN, MIRANDA <1518-22@itmlyceumalabang.onmicrosoft.com>
2026-07-26 12:47:53 +0800

    1.images done 2. saving of quantity done

---
+++ Poketext/Poketext/Migrations/20260726040608_AddQuantityToUserPokemon.cs
@@ -0,0 +1,309 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Poketext.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddQuantityToUserPokemon : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Quantity",
+                table: "UserPokemons",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Pokemons",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ImageUrl",
+                value: "/Images/pokemon/Bulbasaur.png");
+
+            migrationBuilder.UpdateData(
+                table: "Pokemons",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ImageUrl",
+                value: "/Images/pokemon/Ivysaur.png");
+
+            migrationBuilder.UpdateData(
+                table: "Pokemons",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ImageUrl",
+                value: "/Images/pokemon/Venusaur.png");
+
+            migrationBuilder.UpdateData(
+                table: "Pokemons",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ImageUrl",
+                value: "/Images/pokemon/Charmander.png");
+
+            migrationBuilder.UpdateData(
+                table: "Pokemons",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "ImageUrl",
+                value: "/Images/pokemon/Charmeleon.png");
+
+            migrationBuilder.UpdateData(
+                table: "Pokemons",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "ImageUrl",
+                value: "/Images/pokemon/Charizard.png");
+
+            migrationBuilder.UpdateData(
+                table: "Pokemons",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "ImageUrl",
+                value: "/Images/pokemon/Squirtle.png");
+
+            migrationBuilder.UpdateData(
+                table: "Pokemons",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "ImageUrl",
+                value: "/Images/pokemon/Wartortle.png");
+
+            migrationBuilder.UpdateData(
+                table: "Pokemons",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "ImageUrl",
+                value: "/Images/pokemon/Blastoise.png");
+
+            migrationBuilder.UpdateData(
+                table: "Pokemons",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "ImageUrl",
+                value: "/Images/pokemon/Caterpie.png");
+
+            migrationBuilder.UpdateData(
+                table: "Pokemons",
+                keyColumn: "Id",
+                keyValue: 11,
+                column: "ImageUrl",
+                value: "/Images/pokemon/Metapod.png");
+
+            migrationBuilder.UpdateData(
+                table: "Pokemons",
+                keyColumn: "Id",
+                keyValue: 12,
+                column: "ImageUrl",
+                value: "/Images/pokemon/Butterfree.png");
+
+            migrationBuilder.UpdateData(
+                table: "Pokemons",
+                keyColumn: "Id",
+                keyValue: 13,
+                column: "ImageUrl",
+                value: "/Images/pokemon/Pikachu.png");
+
+            migrationBuilder.UpdateData(
+                table: "Pokemons",
+                keyColumn: "Id",
+                keyValue: 14,
+                column: "ImageUrl",
+                value: "/Images/pokemon/Raichu.png");
+
+            migrationBuilder.UpdateData(
+                table: "Pokemons",
+                keyColumn: "Id",
+                keyValue: 15,
+                column: "ImageUrl",
+                value: "/Images/pokemon/Jigglypuff.png");
+
+            migrationBuilder.UpdateData(
+                table: "Pokemons",
+                keyColumn: "Id",
+                keyValue: 16,
+                column: "ImageUrl",
+                value: "/Images/pokemon/Meowth.png");
+
+            migrationBuilder.UpdateData(
+                table: "Pokemons",
+                keyColumn: "Id",
+                keyValue: 17,
+                column: "ImageUrl",
+                value: "/Images/pokemon/Psyduck.png");
+
+            migrationBuilder.UpdateData(
+                table: "Pokemons",
+                keyColumn: "Id",
+                keyValue: 18,
+                column: "ImageUrl",
+                value: "/Images/pokemon/Machop.png");
+
+            migrationBuilder.UpdateData(
+                table: "Pokemons",
+                keyColumn: "Id",
+                keyValue: 19,
+                column: "ImageUrl",
+                value: "/Images/pokemon/Gengar.png");
+
+            migrationBuilder.UpdateData(
+                table: "Pokemons",
+                keyColumn: "Id",
+                keyValue: 20,
+                column: "ImageUrl",
+                value: "/Images/pokemon/Eevee.png");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Quantity",
+                table: "UserPokemons");
+
+            migrationBuilder.UpdateData(
+                table: "Pokemons",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ImageUrl",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Pokemons",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ImageUrl",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Pokemons",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ImageUrl",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Pokemons",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ImageUrl",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Pokemons",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "ImageUrl",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Pokemons",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "ImageUrl",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Pokemons",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "ImageUrl",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Pokemons",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "ImageUrl",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Pokemons",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "ImageUrl",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Pokemons",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "ImageUrl",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Pokemons",
+                keyColumn: "Id",
+                keyValue: 11,
+                column: "ImageUrl",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Pokemons",
+                keyColumn: "Id",
+                keyValue: 12,
+                column: "ImageUrl",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Pokemons",
+                keyColumn: "Id",
+                keyValue: 13,
+                column: "ImageUrl",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Pokemons",
+                keyColumn: "Id",
+                keyValue: 14,
+                column: "ImageUrl",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Pokemons",
+                keyColumn: "Id",
+                keyValue: 15,
+                column: "ImageUrl",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Pokemons",
+                keyColumn: "Id",
+                keyValue: 16,
+                column: "ImageUrl",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Pokemons",
+                keyColumn: "Id",
+                keyValue: 17,
+                column: "ImageUrl",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Pokemons",
+                keyColumn: "Id",
+                keyValue: 18,
+                column: "ImageUrl",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Pokemons",
+                keyColumn: "Id",
+                keyValue: 19,
+                column: "ImageUrl",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Pokemons",
+                keyColumn: "Id",
+                keyValue: 20,
+                column: "ImageUrl",
+                value: "");
+        }
+    }
+}
